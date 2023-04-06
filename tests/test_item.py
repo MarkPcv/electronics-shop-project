bf4a2ebb563a1435c978808d24bf19784dac1f8f
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-04-06 09:59:38 +0300

    Update test_item.py: add tests for new methods

--- tests/test_item.py
+++ tests/test_item.py
@@ -43,3 +43,39 @@ def test_apply_discount(item_book):
     item_book.pay_rate = 0.65
     item_book.apply_discount()
     assert item_book.price == 325
+
+
+def test_instantiate_from_csv():
+    """
+    Checks that all instances from csv file has been created
+    """
+    Item.all = []
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_string_to_number(item_bag):
+    """
+    Checks conversion of string to integer
+    """
+    assert item_bag.string_to_number('7') == 7
+    assert item_bag.string_to_number('7.0') == 7
+    assert item_bag.string_to_number('7.5') == 7
+
+
+def test_name_getter(item_bag):
+    """
+    Checks name getter
+    """
+    assert item_bag.name == 'Bag'
+
+
+def test_name_setter(item_bag):
+    """
+    Checks name setter
+    """
+    item_bag.name = 'Box'
+    assert item_bag.name == 'Box'
+    # Check exception for >10 chars
+    item_bag.name = 'SuperPuperBag'
+    assert item_bag.name == 'Box'
